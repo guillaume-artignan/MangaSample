37022901b9c45dcb0201f78fff901800255b59d1
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-06-15 10:25:51 +0200

    Exemple WS et Manga

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Manga extends Component{
+
+  render(){
+    return <ul className="Manga">
+      
+        <li className="titre">{this.props.titre}</li>
+        <li className="image"><img src={this.props.image} /></li>              
+        <li className="description">{this.props.description}</li>              
+        <li className="categorie">{this.props.categorie}</li>     
+                
+    </ul>;
+  }
+}
+
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {};
+    this.state.data = null;
+    this.state.page = 0;
+  }
+
+  componentDidMount(){
+    this.appelWS(this.state.page);
+  }
+
+  appelWS(page){
+    fetch("http://92.222.69.104/mangas?size=3&page="+page)
+      .then((d)=> d.json())
+      .then((data)=> this.mangasLoaded(data))
+  }
+
+  mangasLoaded(data){
+    console.log(data);
+    this.setState({"data" : data});
+  }
+
+  prevPage(){
+    this.setState({page : this.state.page-1});
+
+    this.appelWS(this.state.page-1);
+  }
+
+  nextPage(){
+    this.setState({page : this.state.page+1});
+    this.appelWS(this.state.page+1);
+  }
+
+  render(){
+    
+    if (this.state.data){
+      var content = this.state.data.content;
+      return (
+        <div className="nav">
+          <button disabled={this.state.data.first} onClick={()=> this.prevPage()}>&lt;</button>
+          <button disabled={this.state.data.last} onClick={()=> this.nextPage()}>&gt;</button>
+          {this.state.page+1} / {this.state.data.totalPages} 
+        <div className="App">
+            {
+              content.map((item,i)=> 
+                <Manga key={i} titre={item.titre} image={item.image} description={item.description} categorie={item.categorie}></Manga>
+              )
+            }
+
+            
+        </div>
+        </div>
+      );
+    }else {
+      return (
+        <div className="App">
+          En cours de chargement ...
+        </div>
+      );
+    }
+
+    
+  }
 }
 
 export default App;
